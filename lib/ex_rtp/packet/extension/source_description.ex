c16cf9f798cc896bb844f1e0bd6afdb25d0f6905
Łukasz Wala <lukasz.wala@swmansion.com>
2023-11-08 13:47:56 +0100

    Add SDES header extension type

---
+++ lib/ex_rtp/packet/extension/source_description.ex
@@ -0,0 +1,47 @@
+defmodule ExRTP.Packet.Extension.SourceDescription do
+  @moduledoc """
+  Source Description (SDES) Extension described in `RFC 7941`.
+  """
+
+  alias ExRTP.Packet.Extension
+
+  @behaviour Extension
+
+  @typedoc """
+  Struct representing SDES Extension.
+
+  The `type` field is is purely informational and ignored, when encoding/decoding the extension,
+  as the SDES item types are determined by the header extension ID value, which is dynamically
+  assigned in signaling and passed to the `to_raw/2` function.
+  """
+  @type t() :: %__MODULE__{
+          type: atom() | nil,
+          text: binary()
+        }
+
+  @enforce_keys [:text]
+  defstruct [:type] ++ @enforce_keys
+
+  @doc """
+  Create new `t:ExRTP.Packet.Extension.SourceDescription.t/0` struct.
+  """
+  @spec new(binary(), atom() | nil) :: t()
+  def new(text, type \\ nil) do
+    %__MODULE__{
+      type: type,
+      text: text
+    }
+  end
+
+  @impl true
+  def from_raw(%Extension{data: data}) do
+    %__MODULE__{
+      text: data
+    }
+  end
+
+  @impl true
+  def to_raw(%__MODULE__{text: text}, id) do
+    Extension.new(id, text)
+  end
+end
---
+++ test/ex_rtp/packet/extension/source_description_test.exs
@@ -0,0 +1,31 @@
+defmodule ExRTP.Packet.Extension.SourceDescriptionTest do
+  use ExUnit.Case, async: true
+
+  alias ExRTP.Packet.Extension
+  alias ExRTP.Packet.Extension.SourceDescription
+
+  @text "hello"
+  @item_type :mid
+
+  test "new/2" do
+    extension = SourceDescription.new(@text)
+    assert %SourceDescription{text: @text, type: nil} = extension
+
+    extension = SourceDescription.new(@text, @item_type)
+    assert %SourceDescription{text: @text, type: @item_type} = extension
+  end
+
+  test "from_raw/1" do
+    raw = %Extension{id: 12, data: @text}
+    extension = SourceDescription.from_raw(raw)
+
+    assert %SourceDescription{text: @text, type: nil} = extension
+  end
+
+  test "to_raw/2" do
+    extension = %SourceDescription{text: @text, type: @item_type}
+    raw = SourceDescription.to_raw(extension, 5)
+
+    assert %Extension{id: 5, data: @text} = raw
+  end
+end
